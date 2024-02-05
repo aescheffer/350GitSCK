c7b3ca062db49313d06714c97637b06e1e823f75
Allison Scheffer <schefallie@gmail.com>
2024-02-05 18:48:38 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import tkinter
+from tkinter import *
+
+window = tkinter.Tk()
+# set window size using geometry funct --> in pixels
+window.geometry("350x200")
+
+# to rename the title of the window
+window.title("GUI")
+
+# pack is used to show the object in the window
+#label = tkinter.Label(window, text = "Hello World!", font=("Arial Bold", 5git 0)).pack()
+
+#create button --> grid function sets button position
+#bg = background color, fg = foreground color
+# bt = Button(window,text="Enter", bg="blue",fg="red")
+# bt.grid(column=1,row=0)
+
+#add button click event DOESNT WORK
+# def clicked(l1):
+#    l1.configure(text="Button was clicked!!")
+# bt=Button(window,text="enter",command=clicked)
+
+#makes button that will make program stop running
+#bd is border width pixels
+btn = Button(window, text="Destroy", bd="15", command = window.destroy)
+btn.pack(side="bottom")
+
+#this makes the window appear
+window.mainloop()
+
+
