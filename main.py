4c80c472da8cd58e243786cbea2e6728b8e48601
Allison Scheffer <schefallie@gmail.com>
2024-02-15 20:01:48 -0500

    fully functional skeleton of game

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 top = Tk()
-top.geometry("800x800")
-top.config(cursor='circle')
+top.geometry("1000x700")
+#top.config(cursor='circle')
 top.title("Hammy Popper Demo")
 text = 'Choose your character!'
 
@@ -35,6 +35,7 @@ def gob():
    goblet_no.place(x=200,y=300)
    T.config(text="Will you place your name in the goblet of fire?")
 
+#if choose YES for goblet of fire, die
 def explode_by_gob():
    goblet_yes.destroy()
    goblet_no.destroy()
@@ -43,6 +44,7 @@ def explode_by_gob():
    #restart.place(x=100,y=100)
    quit.place(x=100,y=200)
 
+#if choose NO for goblet of fire, choose dragon
 def dragon_choice():
    goblet_yes.destroy()
    goblet_no.destroy()
@@ -53,28 +55,27 @@ def dragon_choice():
                  "don't get a choice! The first test is...\n"
                  "DRAGONS!!! Choose your dragon!")
 
+#if choose hungarian dragon, given choice between gilly weed and shark head for underwater challenge
 def hungarian():
    hungarian.destroy()
    chinese.destroy()
    swedish.destroy()
-   # gilly.place(x=200,y=300)
-   # shark.place(x=100,y=300)
    T.config(text="You were able to defeat the Hungarian Horntail and move on to the next round!\n"
                  "You must find a way to breathe underwater for an hour. What will you use?")
    gilly.place(x=200, y=300)
    shark.place(x=500, y=300)
 
+#if choose chinese dragon, given choice between o2 bubble or mystery potion
 def chinese():
    hungarian.destroy()
    chinese.destroy()
    swedish.destroy()
-   # bubble.place(x=200,y=300)
-   # mystery.place(x=100,y=300)
    T.config(text="You defeated the Chinese Fireball! Moving on to the underwater challenge,\n"
                  "what will you use to breathe underwater for an hour?")
    bubble.place(x=200, y=300)
    mystery.place(x=500, y=300)
 
+#if choose swedish dragon, die
 def swedish_death():
    hungarian.destroy()
    chinese.destroy()
@@ -83,21 +84,120 @@ def swedish_death():
                  "lives were tied, your death was assigned to your cousin Vikram III.\n"
                  "Your memory will not live on.")
 
-def ron_and_frenchie():
-   return
-
-def hermione():
-   return
-
-def cho():
-   return
-
+#if choose mystery potion, die from bleach poisoning
 def bleach_death():
-   return
+   bubble.destroy()
+   mystery.destroy()
+   T.config(text="The mystery potion turns out to be bleach supplied by Draco Malfoy. You jump into the water with\n"
+                 "full confidence but are never seen again. No one notices that there is one less contestant.")
+   quit.place(x=200,y=300)
 
+#if choose gilly weed, save Ron and French girl
+def ron_and_frenchie():
+   gilly.destroy()
+   shark.destroy()
+   T.config(text="Great choice! The Gilly Weed gave you flippers, making you fast enough to save\n"
+                 "Ron Weasley and a sickly french child. You move on to the maze! Before you enter,\n"
+                 "will you pick the left or right path?")
+   left.place(x=200,y=300)
+   right.place(x=500,y=300)
 
+#if choose shark head spell, save hermione
+def hermione():
+   gilly.destroy()
+   shark.destroy()
+   T.config(text="The shark head spell gave you the confidence to rescue the brightest witch in your school:\n"
+                 "Hermione Granger. She hints that \"the safe path doesn't always feel right.\" You have reached\n"
+                 "the final challenge of the maze...which path will you choose?")
+   left.place(x=200,y=300)
+   right.place(x=500,y=300)
 
+#if choose bubble spell, save cho
+def cho():
+   bubble.destroy()
+   mystery.destroy()
+   T.config(text="The bubble spell gave you the confidence to save the Ravenclaw, Cho! She gives you the advice that, when it\n "
+            "comes to navigating the maze, \"take the high road.\" You misinterpret her advice and cheat by climbing over \n"
+            "the hedges until you're caught by a soul-sucking dementor! What do you do?")
+   patronus.place(x=200,y=300)
+   dem_run.place(x=500,y=300)
 
+#cast patronus charm on dementor
+def patronus():
+   patronus.destroy()
+   dem_run.destroy()
+   T.config(text="You summon your happiest memory and cast the Patronus charm. Out of your wand appears a massive translucent\n"
+                 "slug that overpowers the dementor. The patronus also reveals the Tri Wizard Cup right in front of you, but\n"
+                 "Viktor Krum has spotted it first and he raises his wand to attack you! What spell will you cast to\n"
+                 "defend yourself?")
+   expel.place(x=200,y=300)
+   avis.place(x=500,y=300)
+
+#run from the dementor
+def dem_run():
+   patronus.destroy()
+   dem_run.destroy()
+   T.config(text="Fueled by fear, you attempt to run from the dementor but onlly manage to fall a great height from\n"
+                 "atop the hedges and break your neck, dying instantly. Too bad there's no spell to fix that!")
+   quit.place(x=200,y=300)
+
+#take left path in the maze
+def left():
+   left.destroy()
+   right.destroy()
+   T.config(text="You head down the left path and before you know it, you've found the cup! And so has Cedric! You grab it\n"
+                 "together and are transported to...a cemetery? And the dark wizard Voldemort is here too?!?! What\n"
+                 "ever will you do???")
+   fight.place(x=200,y=300)
+   vol_run.place(x=500,y=300)
+
+#duel Voldemort --> Win
+def fight_voldy():
+   fight.destroy()
+   vol_run.destroy()
+   T.config(text="Using Cedric as a human shield, you \"dodge\" Voldemort's cast of the death curse. You ultimately\n"
+                 "defeat the dark wizard and return to Hogwarts as the champion of the Tri Wizard Tournament!!!\n"
+                 "THE END")
+
+#run away from Voldemort in the cemetery
+def run_voldy():
+   fight.destroy()
+   vol_run.destroy()
+   T.config(text="You run as fast as you can in the opposite direction of the dark lord. Because you didn't check where\n"
+                 "you were headed, you fall into an open grave. Cedric reaches down to help you out, but is killed by\n"
+                 "Voldemort, falling on top of you and leaving you to suffocate in darkness under the corpse of your friend.\n"
+                 "THE END")
+
+#right path in the maze
+def right():
+   left.destroy()
+   right.destroy()
+   T.config(text="You walk down the right path and spot the Tri Wizard Cup! As you approach the cup, you suddenly \n"
+                 "lock eyes with a bewitched Viktor Krum! You both race toward the cup and you have time to cast \n"
+                 "one spell. Unfortunately, you only remember two...which do you pick?")
+   expel.place(x=200,y=300)
+   avis.place(x=500,y=300)
+
+#defence from Viktor Krum
+def avis():
+   expel.destroy()
+   avis.destroy()
+   T.config(text="You cast the Avis spell, summoning a small flock of birds. Viktor is unfazed and the birds take more\n"
+                 "interest in the berries growing from the hedges than coming to your aid. Viktor body slams you into the\n"
+                 "ground where vines engulf your body and drag you deeper into the maze. You die cold and utterly alone.")
+   quit.place(x=200,y=300)
+
+#defence from Viktor Krum
+def expelliarmus():
+   expel.destroy()
+   avis.destroy()
+   T.config(text="You cast Expelliarmus, unarming Viktor of his wand. As you clutch one handle of the cup, you notice\n"
+                 "that Cedric Diggory has grasped the other at the same time. Together, you are transported to a\n"
+                 "cemetery where Voldemort himself has been waiting! What will you do???")
+   run_voldy.place(x=200,y=300)
+   fight_voldy.place(x=500,y=300)
+
+#Label changes depending on where you are in the game
 T = Label(top, text=text)
 T.place(x=200,y=200)
 
@@ -127,9 +227,21 @@ gilly = Button(top, text="Gilly Weed", command = ron_and_frenchie)
 shark = Button(top, text="Shark Head Spell", command= hermione)
 #
 bubble = Button(top, text="Oxygen Bubble Spell", command=cho)
-mystery = Button(top, text="Mystery Potion", command=bleach_death)
+mystery = Button(top, text="Mystery Potion from your Slytherin \"friends\"", command=bleach_death)
 
-#
+#Maze options
+left = Button(top, text="Left", command=left)
+right = Button(top, text="Right", command=right)
+
+expel = Button(top,text="Expelliarmus",command=expelliarmus)
+avis = Button(top,text="Avis",command=avis)
+
+patronus = Button(top,text="Patronus Charm",command=patronus)
+dem_run = Button(top,text="Run for your life!",command=dem_run)
+
+#Final choice
+fight = Button(top,text="Duel Voldemort",command=fight_voldy)
+vol_run = Button(top,text="RUN!!!",command=run_voldy)
 
 
 
