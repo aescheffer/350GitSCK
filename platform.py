d913e3be59ae9b730958da569adb74f9e4f52b6c
Connor Chambers <chambeco@mail.gvsu.edu>
2024-03-19 18:40:59 -0400

    added platform file

---
+++ platform.py
@@ -0,0 +1,171 @@
+import pygame
+from pygame.locals import *
+
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width=750
+screen_height=750
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("Battle Voldemort")
+
+#game variables
+tile_size = 50
+
+#images
+sun_img = pygame.image.load('img/sunn.png')
+background_img = pygame.image.load('img/skyy.png')
+
+class Player():
+    def __init__(self,x,y):
+        img = pygame.image.load('img/wizard.png')
+        self.image = pygame.transform.scale(img, (40,80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.vel_y = 0
+        self.jumped = False
+
+    def update(self):
+
+        dx = 0
+        dy = 0
+        #get keypresses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -15
+            self.jumped = True
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            dx -= 5
+        if key[pygame.K_RIGHT]:
+            dx += 5
+
+        #add gravity
+        self.vel_y += 1
+        if self.vel_y >10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        #check for collision
+        for tile in world.tile_list:
+            #check for coll in x
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0
+            #check for collision in y direction
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                #check if below the ground (jumping)
+                if self.vel_y < 0:
+                    dy = tile[1].bottom - self.rect.top
+                    self.vel_y = 0
+                # check if above the ground (falling)
+                elif self.vel_y >= 0:
+                    dy = tile[1].top - self.rect.bottom
+                    self.vel_y = 0
+
+        #update player cord.
+        self.rect.x += dx
+        self.rect.y += dy
+
+        # if self.rect.bottom > screen_height:
+        #     self.rect.bottom = screen_height
+        #     dy = 0
+
+        #draw wizard
+        screen.blit(self.image, self.rect)
+
+
+
+class World():
+    def __init__(self,data):
+        self.tile_list = []
+
+        #load images
+        dirt_img = pygame.image.load('img/dirt.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size,tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    dementor = Enemy(col_count * tile_size, row_count * tile_size)
+                    dementor_group.add(dementor)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0],tile[1])
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/dementor.png')
+        self.image = pygame.transform.scale(self.image, (40, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+
+    def update(self):
+        self.rect.x += self.move_direction
+
+world_data = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,2,2,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+]
+
+player = Player(100, screen_height - 400)
+
+dementor_group = pygame.sprite.Group()
+
+world = World(world_data)
+
+run = True
+while run:
+
+    clock.tick(fps)
+    screen.blit(background_img,(0,0))
+    screen.blit(sun_img, (-500, -500))
+
+    world.draw()
+
+    dementor_group.update()
+    dementor_group.draw(screen)
+
+    player.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
