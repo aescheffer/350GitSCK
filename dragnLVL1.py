22854190486d2b079bb80551662c29de9be19dd4
Connor Chambers <chambeco@mail.gvsu.edu>
2024-03-28 16:26:18 -0400

    hope this works

--- dragnLVL1.py
+++ dragnLVL1.py
@@ -16,10 +16,21 @@ pygame.display.set_caption("Battle Voldemort")
 #game variables
 tile_size = 50
 game_over = 0
+level = 0
+max_levels = 1
 
 #images
 sun_img = pygame.image.load('img/sunn.png')
 background_img = pygame.image.load('img/skyy.png')
+
+def reset_level(level):
+    player.reset(100, screen_height-100)
+    dementor_group.empty()
+    dragon_group.empty()
+    exit_group.empty()
+    worlds = [world_data, world_data2]
+    world = World(worlds[level])
+    return world
 
 class Player():
     def __init__(self,x,y):
@@ -81,8 +92,13 @@ class Player():
             if pygame.sprite.spritecollide(self, dementor_group, False):
                 game_over = -1
 
+            if pygame.sprite.spritecollide(self, dragon_group, False):
+                game_over = -1
+
+            #collision with cup
             if pygame.sprite.spritecollide(self, exit_group, False):
                 game_over = 1
+
 
 
             #update player cord.
@@ -103,16 +119,29 @@ class Player():
                 screen.blit(gameOverImg, (225,300))
 
 
-        elif game_over == 1:
-            winImg = pygame.image.load('img/youwin.jpg')
-            winImg = pygame.transform.scale(winImg, (300, 300))
-            screen.blit(winImg, (225, 250))
+        # elif game_over == 1:
+        #     winImg = pygame.image.load('img/youwin.jpg')
+        #     winImg = pygame.transform.scale(winImg, (300, 300))
+        #     screen.blit(winImg, (225, 250))
 
         #draw wizard
         screen.blit(self.image, self.rect)
 
         return game_over
 
+    def reset(self,x,y):
+        img = pygame.image.load('img/wizard.png')
+        dead_image = pygame.image.load('img/ghost.png')
+        self.image = pygame.transform.scale(img, (40, 80))
+        self.dead_image = pygame.transform.scale(dead_image, (40, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.vel_y = 0
+        self.jumped = False
+        self.in_air = True
 
 
 class World():
@@ -139,6 +168,12 @@ class World():
                 if tile == 3:
                     exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size//2))
                     exit_group.add(exit)
+                if tile == 4:
+                    dragon = Dragon(col_count * tile_size, row_count * tile_size)
+                    dragon_group.add(dragon)
+                if tile == 5:
+                    egg = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+                    egg_group.add(egg)
                 col_count += 1
             row_count += 1
 
@@ -164,10 +199,45 @@ class Enemy(pygame.sprite.Sprite):
             self.move_direction *= -1
             self.move_counter *= -1
 
+class Dragon(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/dragon.png')
+        self.image = pygame.transform.scale(self.image, (400, 400))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 5
+        self.move_counter = 0
+
+    def update(self):
+        self.checker = 1
+        if self.checker == 1:
+            self.rect.x += self.move_direction
+            self.rect.y += self.move_direction
+            self.move_counter += 1
+        elif self.checker == -1:
+            self.rect.x -= self.move_direction
+            self.rect.y -= self.move_direction
+            self.move_counter += 1
+        if abs(self.move_counter) > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
+            self.checker *= -1
+
 class Exit(pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load('img/trophy.png')
+        self.image = pygame.transform.scale(img, (tile_size, int(tile_size*1.5)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+class Egg(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/egg.png')
         self.image = pygame.transform.scale(img, (tile_size, int(tile_size*1.5)))
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -181,9 +251,9 @@ world_data = [
     [0,0,0,0,0,1,1,1,1,1,1,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,0,1,0,0,0,4,0,0,0,0,0,0,0],
     [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,2,2,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,1,1,1,1,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
@@ -191,12 +261,35 @@ world_data = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
 
+world_data2 = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,0,0,0,0,0,0,3,0,0],
+    [1,0,0,0,0,0,0,2,0,0,0,0,1,0,0],
+    [1,0,0,0,0,1,1,1,1,1,1,0,0,0,0],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,1,0,0,0,0,0,0,0,3,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+]
+
+
 player = Player(100, screen_height - 400)
 
 dementor_group = pygame.sprite.Group()
 exit_group = pygame.sprite.Group()
+dragon_group = pygame.sprite.Group()
+egg_group = pygame.sprite.Group()
 
-world = World(world_data)
+worlds = [world_data, world_data2]
+world = World(worlds[level])
+#world2= World(world_data2)
 
 run = True
 while run:
@@ -208,14 +301,28 @@ while run:
     world.draw()
 
     if game_over == 0:
+        dragon_group.update()
         dementor_group.update()
 
 
 
     dementor_group.draw(screen)
+    dragon_group.draw(screen)
     exit_group.draw(screen)
 
     game_over = player.update(game_over)
+
+    if game_over == 1:
+        level += 1
+        if level <= max_levels:
+            worlds = []
+            world = reset_level(level)
+            game_over = 0
+        else:
+            winImg = pygame.image.load('img/youwin.jpg')
+            winImg = pygame.transform.scale(winImg, (300, 300))
+            screen.blit(winImg, (225, 250))
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
