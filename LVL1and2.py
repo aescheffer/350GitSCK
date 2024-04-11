020c70884752ddc4a55c04e1ee30c37167bf0546
Allison Scheffer <schefallie@gmail.com>
2024-04-11 18:38:34 -0400

    combined all player classes into one class

--- LVL1and2.py
+++ LVL1and2.py
@@ -3,12 +3,12 @@ from pygame.locals import *
 
 #first level character is a wizard
 class Player():
-    def __init__(self,x,y):
-        self.reset(x,y)
+    def __init__(self,x,y,img,scale):
+        self.reset(x,y,img,scale)
 
     #method to allow player to move around and have consequences for collisions with
     #world and enemies
-    def update(self, game_over):
+    def update1(self, game_over):
         #variables to record change in x and y coordinates
         dx = 0
         dy = 0
@@ -81,11 +81,159 @@ class Player():
 
         return game_over
 
+    def update2(self, game_over):
+        #changes in x and y position depending on player input (keys pressed)
+        dx = 0
+        dy = 0
+
+        if game_over == 0:
+            #get pressed keys
+            key = pygame.key.get_pressed()
+            if key[pygame.K_LEFT]:
+                dx -= 5
+                right = pygame.image.load('img/scuba_right.png')
+                self.img = pygame.transform.scale(right, (60, 70))
+            if key[pygame.K_RIGHT]:
+                dx += 5
+                left = pygame.image.load('img/scuba_man.png')
+                self.img = pygame.transform.scale(left, (60, 70))
+            if key[pygame.K_SPACE] and self.jumped == False:
+                self.vel_y = -10
+                self.jumped = True
+            if key[pygame.K_SPACE] == False:
+                self.jumped = False
+
+            #make gravity
+            self.vel_y += 1
+            if self.vel_y > 3:
+                self.vel_y = 3
+            dy += self.vel_y
+
+            #checking for collision
+            for tile in world2.tileList:
+                #x direction
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+                #y direction
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    #check if jumping
+                    if self.vel_y < 0:
+                        dy = tile[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    #falling
+                    elif self.vel_y >= 0:
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
+
+            #check mermaid collision
+            if pygame.sprite.spritecollide(self, mermaid_group, False):
+                game_over = -1
+
+            #check collision with second mermaid enemy group
+            if pygame.sprite.spritecollide(self, exit_group2, False):
+                game_over = 1
+
+            #add x and y position changes to ending character location
+            self.rect.x += dx
+            self.rect.y += dy
+            #pygame.draw.rect(screen, "white", self.rect, width=1)
+
+        #if the game is over, change character image to ghost and stop the game
+        elif game_over == -1:
+            self.img = self.dead_image
+            if self.rect.y > 100:
+                self.rect.y -= 3
+
+        #draw player onto the screen
+        screen.blit(self.img, self.rect)
+        return game_over
+
+    def update3(self, game_over):
+        #calculate new player position
+        #check collision at new position
+        #adjust player position
+
+        if game_over == 0:
+            #dx and dy represent the change in the x and y variables (start and end position)
+            dx = 0
+            dy = 0
+
+            #get keypresses
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE] and self.jumped == False:
+                self.vel_y = -15
+                self.jumped = True
+            #set jumped back to false
+            if key[pygame.K_SPACE] == False:
+                self.jumped = False
+            if key[pygame.K_LEFT]:
+                dx -= 5
+            if key[pygame.K_RIGHT]:
+                dx += 5
+
+            #add gravity
+            self.vel_y += 1
+            if self.vel_y > 10:
+                self.vel_y = 10
+            dy += self.vel_y
+
+            #check for collision
+            for tile in world3.tile_list:
+                #check for collision in x direction
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+                #check for collision in y direction
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    #check if below the ground (player is jumping)
+                    if self.vel_y < 0:
+                        #player will move until he bumps his head
+                        dy = tile[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    elif self.vel_y >= 0:
+                        #player will move until he falls
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
+            #check for collision with enemies
+            if pygame.sprite.spritecollide(self, bad_group, False):
+                game_over = -1
+            if pygame.sprite.spritecollide(self, exit_group3, False):
+                game_over = 1
+
+            #update player coordinates
+            self.rect.x += dx
+            self.rect.y += dy
+
+        #if game is over/enemy collides with player, player image becomes ghost and game stops
+        elif game_over == -1:
+            self.image = self.dead_image
+            if self.rect.y > 200:
+                self.rect.y -= 5
+            else:
+                #place game over image on screen
+                gameOverImg = pygame.image.load('img/gameover.jpg')
+                gameOverImg = pygame.transform.scale(gameOverImg,(300,300))
+                screen.blit(gameOverImg, (225,300))
+
+        #if player3 reaches final trophy, display final winning image
+        elif game_over == 1:
+            winImg = pygame.image.load('img/youwin.jpg')
+            winImg = pygame.transform.scale(winImg, (300, 300))
+            screen.blit(winImg, (225, 250))
+
+
+
+        #draw player onto screen
+        screen.blit(self.image, self.rect)
+        #pygame.draw.rect(screen, (255,255,255), self.rect, 2)
+
+        return game_over
+
+
     #reset character position and image if restart button is pressed
-    def reset(self,x,y):
-        img = pygame.image.load('img/knighty.png')
+    def reset(self,x,y, img, scale):
+        # img = pygame.image.load('img/knighty.png')
         dead_image = pygame.image.load('img/ghost.png')
-        self.image = pygame.transform.scale(img, (40, 80))
+        self.image = pygame.transform.scale(img, scale)
         self.dead_image = pygame.transform.scale(dead_image, (40, 80))
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -95,6 +243,8 @@ class Player():
         self.vel_y = 0
         self.jumped = False
         self.in_air = True
+
+
 
 
 #first level world creation class
@@ -177,93 +327,6 @@ class Exit(pygame.sprite.Sprite):
 
 
 '''NEW MAP CLASSES FOR LEVEL 2'''
-
-
-
-#second level player image and rules for movement
-class Player2():
-    def __init__(self, x, y):
-        self.reset(x,y)
-
-    def update(self, game_over):
-        #changes in x and y position depending on player input (keys pressed)
-        dx = 0
-        dy = 0
-
-        if game_over == 0:
-            #get pressed keys
-            key = pygame.key.get_pressed()
-            if key[pygame.K_LEFT]:
-                dx -= 5
-                right = pygame.image.load('img/scuba_right.png')
-                self.img = pygame.transform.scale(right, (60, 70))
-            if key[pygame.K_RIGHT]:
-                dx += 5
-                left = pygame.image.load('img/scuba_man.png')
-                self.img = pygame.transform.scale(left, (60, 70))
-            if key[pygame.K_SPACE] and self.jump == False:
-                self.vely = -10
-                self.jump = True
-            if key[pygame.K_SPACE] == False:
-                self.jump = False
-
-            #make gravity
-            self.vely += 1
-            if self.vely > 3:
-                self.vely = 3
-            dy += self.vely
-
-            #checking for collision
-            for tile in world2.tileList:
-                #x direction
-                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                    dx = 0
-                #y direction
-                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                    #check if jumping
-                    if self.vely < 0:
-                        dy = tile[1].bottom - self.rect.top
-                        self.vely = 0
-                    #falling
-                    elif self.vely >= 0:
-                        dy = tile[1].top - self.rect.bottom
-                        self.vely = 0
-
-            #check mermaid collision
-            if pygame.sprite.spritecollide(self, mermaid_group, False):
-                game_over = -1
-
-            #check collision with second mermaid enemy group
-            if pygame.sprite.spritecollide(self, exit_group2, False):
-                game_over = 1
-
-            #add x and y position changes to ending character location
-            self.rect.x += dx
-            self.rect.y += dy
-            #pygame.draw.rect(screen, "white", self.rect, width=1)
-
-        #if the game is over, change character image to ghost and stop the game
-        elif game_over == -1:
-            self.img = self.deadimg
-            if self.rect.y > 100:
-                self.rect.y -= 3
-
-        #draw player onto the screen
-        screen.blit(self.img, self.rect)
-        return game_over
-
-    #resets character position when restart button is pressed by user
-    def reset(self, x, y):
-        img = pygame.image.load('img/wizard.png')
-        self.deadimg = pygame.transform.scale(pygame.image.load('img/ghost.png'), (40,70))
-        self.img = pygame.transform.scale(img, (40, 70))
-        self.rect = self.img.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.vely = 0
-        self.jump = False
-        self.width = self.img.get_width()
-        self.height = self.img.get_height()
 
 
 
@@ -375,105 +438,6 @@ class Mermaids3(pygame.sprite.Sprite):
 
 
 
-#third player corresponding to the third and final level of the game
-class Player3():
-    def __init__(self, x, y):
-        self.reset(x,y)
-
-    def update(self, game_over):
-        #calculate new player position
-        #check collision at new position
-        #adjust player position
-
-        if game_over == 0:
-            #dx and dy represent the change in the x and y variables (start and end position)
-            dx = 0
-            dy = 0
-
-            #get keypresses
-            key = pygame.key.get_pressed()
-            if key[pygame.K_SPACE] and self.jumped == False:
-                self.vel_y = -15
-                self.jumped = True
-            #set jumped back to false
-            if key[pygame.K_SPACE] == False:
-                self.jumped = False
-            if key[pygame.K_LEFT]:
-                dx -= 5
-            if key[pygame.K_RIGHT]:
-                dx += 5
-
-            #add gravity
-            self.vel_y += 1
-            if self.vel_y > 10:
-                self.vel_y = 10
-            dy += self.vel_y
-
-            #check for collision
-            for tile in world3.tile_list:
-                #check for collision in x direction
-                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                    dx = 0
-                #check for collision in y direction
-                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                    #check if below the ground (player is jumping)
-                    if self.vel_y < 0:
-                        #player will move until he bumps his head
-                        dy = tile[1].bottom - self.rect.top
-                        self.vel_y = 0
-                    elif self.vel_y >= 0:
-                        #player will move until he falls
-                        dy = tile[1].top - self.rect.bottom
-                        self.vel_y = 0
-            #check for collision with enemies
-            if pygame.sprite.spritecollide(self, bad_group, False):
-                game_over = -1
-            if pygame.sprite.spritecollide(self, exit_group3, False):
-                game_over = 1
-
-            #update player coordinates
-            self.rect.x += dx
-            self.rect.y += dy
-
-        #if game is over/enemy collides with player, player image becomes ghost and game stops
-        elif game_over == -1:
-            self.image = self.dead_image
-            if self.rect.y > 200:
-                self.rect.y -= 5
-            else:
-                #place game over image on screen
-                gameOverImg = pygame.image.load('img/gameover.jpg')
-                gameOverImg = pygame.transform.scale(gameOverImg,(300,300))
-                screen.blit(gameOverImg, (225,300))
-
-        #if player3 reaches final trophy, display final winning image
-        elif game_over == 1:
-            winImg = pygame.image.load('img/youwin.jpg')
-            winImg = pygame.transform.scale(winImg, (300, 300))
-            screen.blit(winImg, (225, 250))
-
-
-
-        #draw player onto screen
-        screen.blit(self.image, self.rect)
-        #pygame.draw.rect(screen, (255,255,255), self.rect, 2)
-
-        return game_over
-
-    #reset player3 position when restart button is pushed by user
-    def reset(self, x, y):
-        img = pygame.image.load('img/wizard.png')
-        self.image = pygame.transform.scale(img, (40,80))
-        dead_image = pygame.image.load('img/ghost.png')
-        self.dead_image = pygame.transform.scale(dead_image, (40,80))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
-        self.vel_y = 0
-        self.jumped = False
-
 #third and final world creation
 class World3():
     def __init__(self, data):
@@ -513,8 +477,7 @@ class World3():
         for tile in self.tile_list:
             #takes picture and puts it in the location of rect coordinates
             screen.blit(tile[0], tile[1])
-            #outlines all images -- helpful for logic
-            #pygame.draw.rect(screen, (255,255,255), tile[1], 2)
+
 
 #enemy class to create all enemies in level 3
 class Enemy2(pygame.sprite.Sprite):
@@ -602,6 +565,15 @@ if __name__ == '__main__':
     max_levels = 2
 
     # images to load in
+    img1 = pygame.image.load('img/knighty.png')
+    scale1 = (40,80)
+    img2 = pygame.image.load('img/scuba_man.png')
+    scale2 = (40,70)
+    img3 = pygame.image.load('img/wizard.png')
+    scale3 = (40,70)
+
+
+
     background_img1 = pygame.image.load('img/arena3.jpg')
     background_img2 = pygame.transform.scale(pygame.image.load('img/underwaterBG.jpg'), (750, 750))
     background_img3 = pygame.transform.scale(pygame.image.load('img/foggy_night.png'), (750, 750))
@@ -664,8 +636,11 @@ if __name__ == '__main__':
     ]
 
     #first level player
-    player = Player(100, screen_height - 400)
+    player = Player(100, screen_height - 400, img1, scale1)
 
+    character = Player(50, 600, img2, scale2)
+
+    player3 = Player(100, screen_height - 130, img3, scale3)
 
     exit_group = pygame.sprite.Group()
     exit_group2 = pygame.sprite.Group()
@@ -677,12 +652,12 @@ if __name__ == '__main__':
 
     mermaid_group = pygame.sprite.Group()
     world2= World2(LVL2)
-    character = Player2(50, 600)
+
 
     bad_group = pygame.sprite.Group()
     exit_group3 = pygame.sprite.Group()
     world3 = World3(LVL3)
-    player3 = Player3(100, screen_height - 130)
+
 
     start = Start()
     # restart_img = pygame.image.load('img/restart_img.png')
@@ -714,7 +689,7 @@ if __name__ == '__main__':
             dragon_group.draw(screen)
             exit_group.draw(screen)
 
-            game_over = player.update(game_over)
+            game_over = player.update1(game_over)
 
             if game_over == 1:
                 level += 1
@@ -730,7 +705,7 @@ if __name__ == '__main__':
             if game_over == -1:
                 if restart_button.draw(screen):
                     level = -1
-                    player.reset(100, screen_height - 200)
+                    player.reset(100, screen_height - 200, img1, scale1)
                     game_over = 0
 
         elif level == 1:
@@ -744,7 +719,7 @@ if __name__ == '__main__':
             mermaid_group.draw(screen)
             exit_group2.draw(screen)
 
-            game_over = character.update(game_over)
+            game_over = character.update2(game_over)
 
             if game_over == 1:
                 level += 1
@@ -753,8 +728,8 @@ if __name__ == '__main__':
             if game_over == -1:
                 if restart_button.draw(screen):
                     level = -1
-                    player.reset(100, screen_height - 200)
-                    character.reset(50, 600)
+                    player.reset(100, screen_height - 200, img1, scale1)
+                    character.reset(50, 600, img2, scale2)
                     game_over = 0
 
         elif level == 2:
@@ -768,14 +743,14 @@ if __name__ == '__main__':
             bad_group.draw(screen)
             exit_group3.draw(screen)
 
-            game_over = player3.update(game_over)
+            game_over = player3.update3(game_over)
 
             if game_over == -1:
                 if restart_button.draw(screen):
                     level = -1
-                    player.reset(100, screen_height - 200)
-                    character.reset(50, 600)
-                    player3.reset(100, screen_height - 130)
+                    player.reset(100, screen_height - 200, img1, scale1)
+                    character.reset(50, 600, img2, scale2)
+                    player3.reset(100, screen_height - 130, img3, scale3)
                     game_over = 0
 
 
